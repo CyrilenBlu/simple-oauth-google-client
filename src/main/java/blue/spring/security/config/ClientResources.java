ce52adb429b3be68d43adaefd53a9002aa93adf0
CyrilenBlu <lukepetzerint2@gmail.com>
2019-04-11 21:43:54 +0200

    Added Multi-OAuth Login

---
+++ src/main/java/blue/spring/security/config/ClientResources.java
@@ -0,0 +1,36 @@
+package blue.spring.security.config;
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+
+@Configuration
+public class ClientResources {
+
+    @Bean
+    public FilterRegistrationBean<OAuth2ClientContextFilter> oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
+        FilterRegistrationBean<OAuth2ClientContextFilter> registration = new FilterRegistrationBean<OAuth2ClientContextFilter>();
+        registration.setFilter(filter);
+        registration.setOrder(-100);
+        return registration;
+    }
+
+    @NestedConfigurationProperty
+    private AuthorizationCodeResourceDetails client = new AuthorizationCodeResourceDetails();
+
+    @NestedConfigurationProperty
+    private ResourceServerProperties resource = new ResourceServerProperties();
+
+    public AuthorizationCodeResourceDetails getClient() {
+        return client;
+    }
+
+    public ResourceServerProperties getResource() {
+        return resource;
+    }
+
+}
--- src/main/java/blue/spring/security/config/WebSecurityConfiguration.java
+++ src/main/java/blue/spring/security/config/WebSecurityConfiguration.java
@@ -2,10 +2,9 @@ package blue.spring.security.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,16 +13,18 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
-import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.filter.CompositeFilter;
 
+import javax.servlet.Filter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configurable
 @EnableWebSecurity
@@ -31,23 +32,19 @@ import java.io.IOException;
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private OAuth2ClientContext clientContext;
-    private AuthorizationCodeResourceDetails resourceDetails;
-    private ResourceServerProperties resourceServerProperties;
 
     @Autowired
     public void setClientContext(OAuth2ClientContext clientContext) {
         this.clientContext = clientContext;
     }
 
-    @Autowired
-    public void setResourceDetails(AuthorizationCodeResourceDetails resourceDetails) {
-        this.resourceDetails = resourceDetails;
-    }
+    @Bean
+    @ConfigurationProperties("google")
+    public ClientResources google() { return new ClientResources(); }
 
-    @Autowired
-    public void setResourceServerProperties(ResourceServerProperties resourceServerProperties) {
-        this.resourceServerProperties = resourceServerProperties;
-    }
+    @Bean
+    @ConfigurationProperties("github")
+    public ClientResources github() { return new ClientResources(); }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -63,21 +60,30 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/")
                 .permitAll()
                 .and()
-                .addFilterAt(filter(), BasicAuthenticationFilter.class)
+                .addFilterAt(ssoFilter(), BasicAuthenticationFilter.class)
                 .csrf()
                 .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
     }
 
-    private OAuth2ClientAuthenticationProcessingFilter filter() {
+    private Filter ssoFilter() {
+        CompositeFilter filter = new CompositeFilter();
+        List<Filter> filters = new ArrayList<>();
+        filters.add(filter(google(), "/login/google"));
+        filters.add(filter(github(), "/login/github"));
+        filter.setFilters(filters);
+        return filter;
+    }
+
+    private OAuth2ClientAuthenticationProcessingFilter filter(ClientResources client, String path) {
 
         OAuth2ClientAuthenticationProcessingFilter oAuth2Filter = new OAuth2ClientAuthenticationProcessingFilter(
-                "/login/google");
+                path);
 
-        OAuth2RestTemplate oAuth2RestTemplate = new OAuth2RestTemplate(resourceDetails, clientContext);
+        OAuth2RestTemplate oAuth2RestTemplate = new OAuth2RestTemplate(client.getClient(), clientContext);
         oAuth2Filter.setRestTemplate(oAuth2RestTemplate);
 
-        oAuth2Filter.setTokenServices(new UserInfoTokenServices(resourceServerProperties.getUserInfoUri(),
-                resourceServerProperties.getClientId()));
+        oAuth2Filter.setTokenServices(new UserInfoTokenServices(client.getResource().getUserInfoUri(),
+                client.getClient().getClientId()));
 
         oAuth2Filter.setAuthenticationSuccessHandler(new SimpleUrlAuthenticationSuccessHandler() {
             public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
